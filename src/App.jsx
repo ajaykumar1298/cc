e67281eda36fbdfa4e48a22a98698a0f932e3d34
Ajay Kumar <ajaykumar@spicegems.com>
2025-03-11 11:41:43 +0530

    update change click event on swap button nd input field

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,14 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [amount,setAmount]=useState(0)
+  let [fromCurr,setFromCurr]=useState("usd")
+  let [toCurr,setToCurr]=useState("inr")
+  let [calAmount,setCalAmount]=useState(null)
+  let swapCurr =()=>{
+    setToCurr(fromCurr)
+  setFromCurr(toCurr)
+  }
 
   return (
   <>
@@ -14,26 +21,30 @@ function App() {
     </div>
     <div className="amount-section">
       <label htmlFor="curr-input">amount</label>
-      <input type="number"  />
+      <input type="number"  value={amount} min={0} onChange={(e)=>setAmount(e.target.value)} />
     </div>
     <div className="currency-section">
       <div className="form-currency">
         <label htmlFor="from">from</label>
-        <select>
+        <select value={fromCurr} onChange={(e)=>setFromCurr(e.target.value)}>
           <option value="usd">usd</option>
+          <option value="EUR">EUR</option>
+          <option value="inr">inr</option>
         </select>
       </div>
-      <button type='text'> swap currency</button>
+      <button type='button' onClick={swapCurr}> swap currency</button>
       <div className="to-currency">
         <label htmlFor="to">to</label>
-        <select>
+        <select value={toCurr} onChange={(e)=>setToCurr(e.target.value)}>
           <option value="inr">inr</option>
+          <option value="EUR">EUR</option>
+          <option value="usd">usd</option>
         </select>
       </div>
     </div>
     <div className="footer-section">
       <button type='button'>Currency Convertor</button>
-      <p> 1 usd is equal to 83 inr</p>
+      {calAmount &&  <p> 1 usd is equal to 83 inr</p>} 
     </div>
   </div>
   </>
